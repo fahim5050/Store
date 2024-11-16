330e2cbd97fe43cbd82aa90d252b50d7cac5f8f6
fahim5050 <85991432+fahim5050@users.noreply.github.com>
2024-11-16 19:50:07 +0500

    add serial no

--- Components/ProductTable/ProductTable.jsx
+++ Components/ProductTable/ProductTable.jsx
@@ -5,28 +5,37 @@ import { StyleSheet, Text, View, FlatList } from "react-native";
 
 const ProductTable = ({ products }) => {
   const navigation = useNavigation();
+
+  // Method to get serial number for each item
+  const getSerialNumber = (index) => {
+    return index + 1; // Adds 1 to index to start from 1
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>All Products</Text>
 
       {/* Column Headers */}
       <View style={styles.row}>
+        <Text style={[styles.text, styles.headerText]}>S.No</Text>
         <Text style={[styles.text, styles.headerText]}>Name</Text>
         <Text style={[styles.text, styles.headerText]}>Price</Text>
         <Text style={[styles.text, styles.headerText]}>Stock</Text>
       </View>
 
       <FlatList
-        data={products} // Now passing products data
+        data={products} // Passing products data
         showsVerticalScrollIndicator={false}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
+        renderItem={({ item, index }) => (
           <TouchableOpacity
             onPress={() =>
               navigation.navigate("ProductDetail", { product: item })
             }
           >
             <View style={styles.row}>
+              {/* Wrap serial number in <Text> */}
+              <Text style={styles.serial}>{getSerialNumber(index)}</Text> 
               <Text style={styles.productName}>{item.name}</Text>
               <Text style={styles.text}>${item.price}</Text>
               <Text style={styles.text}>{item.stockQuantity}</Text>
@@ -42,10 +51,9 @@ export default ProductTable;
 
 const styles = StyleSheet.create({
   container: {
-    // padding: 10,
     backgroundColor: "#fff",
-    width:'100%',
-    marginTop:-30
+    width: "100%",
+    marginTop: -30,
   },
   header: {
     fontSize: 24,
@@ -60,16 +68,22 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#ddd",
   },
-  productName:{
+  productName: {
     fontSize: 16,
     color: "#333",
-    width: "40%", // Adjust width as needed
-    textAlign: "left"
+    width: "30%", // Adjust width as needed
+    textAlign: "left",
   },
   text: {
     fontSize: 16,
     color: "#333",
-    width: "30%", // Adjust width as needed
+    width: "20%", // Adjust width as needed
+    textAlign: "center",
+  },
+  serial:{
+    fontSize: 16,
+    color: "#333",
+    width: "10%", // Adjust width as needed
     textAlign: "center",
   },
   headerText: {
